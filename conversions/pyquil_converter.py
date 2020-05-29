65fb086a638731afa7a6230153d6d60900093f2a
Thomas Wangler <thomas.wangler1996@web.de>
2020-05-29 23:42:51 +0200

    handle gate method

--- conversions/pyquil_converter.py
+++ conversions/pyquil_converter.py
@@ -4,6 +4,7 @@ from pyquil.quilbase import Declare, Gate, Halt, Measurement, Pragma, DefGate
 from qiskit_utility import show_figure
 from gate_mappings import gate_mapping_qiskit, gate_mapping_pyquil
 from qiskit.extensions import UnitaryGate
+from qiskit.circuit import Qubit
 
 
 class PyquilConverter:
@@ -13,75 +14,65 @@ class PyquilConverter:
         return PyquilConverter.import_pyquil(program)
 
     @staticmethod
-    def import_pyquil(program: Program) -> QuantumCircuit:
-        # check if qubit set is from 0 to len(set)
+    def import_pyquil(program: Program) -> (QuantumCircuit, {int: Qubit}, {}):
         qubit_set = program.get_qubits()
         qreg_mapping = {}
-        creg_mapping = {}
-        # all_qubits = False
-        # for i in range(0, len(qubit_set)):
-        #     if not (i in qubit_set):
-        #         all_qubits = False
-        #         break
-        # q = []
-        # if all_qubits:
-        #     q.append(QuantumRegister(len(qubit_set), "q"))
-        # else:
-        #     for i in qubit_set:
-        #         qr = QuantumRegister(1, "q" + str(i))
-        #         q.append(qr)
-        #         qreg_mapping[i] = qr
-        # # *q converts list to single parameters
-        # circuit = QuantumCircuit(*q)
+        creg_mapping = {}        
 
         qr = QuantumRegister(len(qubit_set), "q")
         for counter, qubit in enumerate(qubit_set):
-            qreg_mapping[qubit] = counter
+            qreg_mapping[qubit] = qr[counter]
         circuit = QuantumCircuit(qr)
 
-        for inst in program.instructions:
-            if isinstance(inst, Declare):
-                if inst.memory_type != "BIT":
+        for instr in program.instructions:
+            if isinstance(instr, Declare):
+                if instr.memory_type != "BIT":
                     raise NotImplementedError(
-                        "Unsupported memory type:" + inst.memory_type
-                    )
-                cr = ClassicalRegister(inst.memory_size, inst.name)
-                creg_mapping[inst.name] = cr
+                        "Unsupported memory type:" + str(instr.memory_type))
+
+                cr = ClassicalRegister(instr.memory_size, instr.name)
+                creg_mapping[instr.name] = cr
                 circuit.add_register(cr)
 
-            elif isinstance(inst, Gate):
-                if (inst.name in gate_mapping_pyquil) or (isinstance(inst, DefGate)):
-                    if (inst.name in gate_mapping_pyquil):
-                        # get the instruction
-                        inst_qiskit_class = gate_mapping_pyquil[inst.name]
-                        # TODO check if division by pi is necessary (pytket does this)
-                        params = inst.params
-                        inst_qiskit = inst_qiskit_class(*params)
-                    else:
-                        inst_qiskit = UnitaryGate(inst.matrix)
-                    # get the qubits on which the instruction operates
-                    qubits = [qreg_mapping[qubit.index] for qubit in inst.qubits]
-                    qargs = [qr[qubit] for qubit in qubits]
-                    circuit.append(inst_qiskit, qargs=qargs)
-                
-                else:
-                    raise NotImplementedError("Gate " + str(inst) + " is not supported.")
-        
-            elif isinstance(inst, Measurement):
-                qubit = qr[qreg_mapping[inst.qubit.index]]
-                print(qubit)
-                creg = creg_mapping[inst.classical_reg.name]
-                print(creg)
-                bit = creg[inst.classical_reg.offset]
+            elif isinstance(instr, Gate):
+                PyquilConverter._handle_gate(circuit, instr, qreg_mapping, creg_mapping)
+
+            elif isinstance(instr, Measurement):
+                qubit = qreg_mapping[instr.qubit.index]
+                creg = creg_mapping[instr.classical_reg.name]
+                bit = creg[instr.classical_reg.offset]
                 circuit.measure(qubit, bit)
 
-           
-            # elif isinstance(i, Pragma):
-            #     continue
-            # elif isinstance(i, Halt):
-            #     return tkc
-            # else:
-            #     raise NotImplementedError("PyQuil instruction is not a gate: " + str(i))
+            elif isinstance(instr, Pragma):
+                # http://docs.rigetti.com/en/stable/basics.html#pragmas
+                # pragmas do not change the semantics of a program
+                # e.g. rewiring and delays
+                # can be alternatively implemented with qiskit (https://qiskit.org/documentation/stubs/qiskit.pulse.Delay.html)
+                continue
+            elif isinstance(instr, Halt):
+                break
+            else:
+                raise NotImplementedError(
+                    "Unsupported instruction: " + str(instr))
 
         show_figure(circuit)
-        return
+        return (circuit, qreg_mapping, creg_mapping)
+
+    @staticmethod
+    def _handle_gate(circuit, instr, qreg_mapping, creg_mapping) -> None:        
+        if (instr.name in gate_mapping_pyquil) or (isinstance(instr, DefGate)):
+            if (instr.name in gate_mapping_pyquil):
+                # get the instruction
+                instr_qiskit_class = gate_mapping_pyquil[instr.name]
+                # TODO check if division by pi is necessary (pytket does this)
+                params = instr.params
+                instr_qiskit = instr_qiskit_class(*params)
+            else:
+                instr_qiskit = UnitaryGate(instr.matrix)
+            # get the qubits on which the instruction operates
+            qargs = [qreg_mapping[qubit.index]
+                        for qubit in instr.qubits]
+            circuit.append(instr_qiskit, qargs=qargs)
+
+        else:
+            raise NotImplementedError("Unsupported Gate: " + str(instr))
