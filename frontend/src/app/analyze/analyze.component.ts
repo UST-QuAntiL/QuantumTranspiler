b16bb4e7c5d087faf055d594869599ce043ceeb3
Thomas Wangler <thomas.wangler1996@web.de>
2020-10-05 14:35:05 +0200

    frontend unroll and export in one call

--- frontend/src/app/analyze/analyze.component.ts
+++ frontend/src/app/analyze/analyze.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import { DataService } from '../services/data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-analyze',
@@ -16,8 +17,10 @@ export class AnalyzeComponent implements OnInit {
     "r_gate_times": 0,
     "r_two_qubit": 0
   };
+
+  formatUsed: string;
   
-  constructor(private http: HttpService, private data: DataService) { }
+  constructor(private http: HttpService, private data: DataService, private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
@@ -35,23 +38,39 @@ export class AnalyzeComponent implements OnInit {
 
   useQiskit() {
     this.unroll("IBMQ")
+    this.formatUsed = "qasm"
   }
 
   useRigetti() {
     this.unroll("Rigetti")
+    this.formatUsed = "quil"
   }
 
   private async unroll(option: string) {  
+    this.snackbar.open("Request sent to backend. Results will be available shortly.");
+
     let object = {
       "option": option,
       "circuit": this.data.circuits["internal"],
-      "nativeGates": []
     }
 
-    let circuit = await this.http.callBackend(object, "unroll")
+    let circuit = await this.http.callBackend(object, "unrollToNativeFormat")
     if (circuit) {
-      this.data.setCircuit("unroll", circuit)
+      this.data.setCircuit("export", circuit)
     }
+  }
+
+  download() {
+    if (this.data.circuits[2] === "") {
+      this.snackbar.open("Circuit is empty.");
+      return;
+    }
+    if (!this.formatUsed) {
+      this.snackbar.open("No valid format selected.");
+      return;
+    }
+    var blob = new Blob([this.data.circuits["export"]], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "circuit." + this.formatUsed);
   }
 
 
