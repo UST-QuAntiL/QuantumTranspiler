7679a7c1d6bf5ebae5ce2085be4f38591e2427b5
Thomas Wangler <thomas.wangler1996@web.de>
2020-10-06 12:04:39 +0200

    coupling maps for several architectures specified

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,2 +1,3 @@
 from examples.planqk_examples import *
 from examples.custom_circuits import *
+from examples.qpu_couplings import *
---
+++ examples/qpu_couplings.py
@@ -0,0 +1,56 @@
+
+from qiskit.providers.basebackend import BaseBackend
+from qiskit.providers.ibmq import IBMQ
+from qiskit.transpiler import CouplingMap
+
+# Rigetti Aspen
+def aspen_4():
+    coupling = CouplingMap([
+        [0, 1],
+        [0, 4],
+        [1, 2],
+        [2, 3],
+        [2, 5],
+        [5, 6],
+        [6, 7],
+        [7, 8],
+        [8, 9],
+        [9, 10],
+        [10, 11],
+    ])
+    coupling.make_symmetric()
+    return coupling
+
+
+# IBMQ
+def ibmq(qpu_name: str):
+    provider = IBMQ.load_account()
+    backend = provider.get_backend(qpu_name)
+    return CouplingMap(backend.configuration().coupling_map)
+
+# device names
+def ibmq_qasm_simulator():
+    return ibmq("ibmq_qasm_simulator")
+def ibmqx2():
+    return ibmq("ibmqx2")
+def ibmq_16_melbourne():
+    return ibmq("ibmq_16_melbourne")
+def ibmq_vigo():
+    return ibmq("ibmq_vigo")
+def ibmq_ourense():
+    return ibmq("ibmq_ourense")
+def ibmq_valencia():
+    return ibmq("ibmq_valencia")
+def ibmq_london():
+    return ibmq("ibmq_london")
+def ibmq_essex():
+    return ibmq("ibmq_essex")
+def ibmq_burlington():
+    return ibmq("ibmq_burlington")
+def ibmq_santiago():
+    return ibmq("ibmq_santiago")
+
+if __name__ == "__main__":
+    # map = ibmq_ourense()
+    map = aspen_4()
+    print(map)
