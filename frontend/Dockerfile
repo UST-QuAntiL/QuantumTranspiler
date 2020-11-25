159e615d8b6d73e1fe39c7fd272217fad0736375
Thomas Wangler <thomas.wangler1996@web.de>
2020-11-25 12:33:07 +0100

    frontend build for use in docker

---
+++ frontend/Dockerfile
@@ -0,0 +1,3 @@
+FROM nginx:alpine
+COPY /dist/frontend /usr/share/nginx/html
+EXPOSE 80
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://193.196.39.182:5000/"
+  apiUrl: "http://localhost:5000/"
 };
