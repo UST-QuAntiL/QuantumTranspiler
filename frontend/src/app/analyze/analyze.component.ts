38b296caf26bc9d6cdad3b23af1c116f02b77eea
salmma <marie.salm@iaas.uni-stuttgart.de>
2021-12-10 13:24:45 +0100

    add import

--- frontend/src/app/analyze/analyze.component.ts
+++ frontend/src/app/analyze/analyze.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import { DataService } from '../services/data.service';
+import { saveAs } from 'file-saver';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import {
   trigger,
