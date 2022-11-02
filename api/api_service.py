f9cf02712145e437c94dd3e50ab28589dac8d56d
Maximilian Kuhn <maximilian.kuhn@ymail.com>
2022-11-02 12:40:51 +0100

    Refactored API

--- api/api_service.py
+++ api/api_service.py
@@ -12,255 +12,168 @@ app = Flask(__name__)
 cors = CORS(app)
 
 
-@app.route('/circuit_to_internal', methods=['Post'])
+@app.route("/circuit_to_internal", methods=["Post"])
 def circuit_to_internal():
     data = request.json
-    option = data["option"]
+    language = data["option"]
     circuit = data["circuit"]
     try:
         wrapper = CircuitWrapper()
-        option_lower = option.lower()
-        if option_lower == "quil":
-            wrapper.import_quil(circuit)
-        elif option_lower == "pyquil":
-            wrapper.import_pyquil(circuit)
-        elif option_lower == "openqasm":
-            wrapper.import_qasm(circuit)
-        elif option_lower == "qiskit":
-            wrapper.import_qiskit(circuit)
-        elif option_lower == "cirq":
-            wrapper.import_cirq_json(circuit)
-        elif option_lower == "cirqsdk":
-            wrapper.import_cirq(circuit)
-        elif option_lower == "braket":
-            wrapper.import_braket_ir(circuit)
-        elif option_lower == "braketsdk":
-            wrapper.import_braket(circuit)
-        elif option_lower == "qsharp":
-            wrapper.import_qsharp(circuit)
-        elif option_lower == "quirk":
-            wrapper.import_quirk(circuit)
-        else:
-            return "Bad Request!", 400
+        wrapper.import_language(circuit, language)
         output = wrapper.export_qiskit_commands()
+    except ValueError:
+        return "Bad Request!", 400
     except DeviceError as de:
-        gate = re.findall(r'(?:Gate )(.*?)(?: not)', str(de))[0]
-        return f"Importing {option} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:Gate )(.*?)(?: not)", str(de))[0]
+        return f"Importing {language} does not support '{gate}' gates", 500
     except QasmException as qe:
         gate = re.findall(r'"(.*?)"', qe.message)[0]
-        return f"Importing {option} does not support '{gate}' gates", 500
+        return f"Importing {language} does not support '{gate}' gates", 500
     except NotImplementedError as nie:
-        gate = re.findall(r'(?:convert )(.*?)(?: |\()', str(nie))[0]
-        return f"Importing {option} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:convert )(.*?)(?: |\()", str(nie))[0]
+        return f"Importing {language} does not support '{gate}' gates", 500
     except SyntaxError as se:
-        line = re.findall(r'(line \d+)', str(se))[0]
+        line = re.findall(r"(line \d+)", str(se))[0]
         return f"Invalid syntax of input in line {line}"
     except Exception as e:
         traceback.print_exc()
         print(str(e))
-        return f"General error while importing {option}: {str(e)}", 500
+        return f"General error while importing {language}: {str(e)}", 500
     return output
 
 
-@app.route('/export_circuit', methods=['Post'])
+@app.route("/export_circuit", methods=["Post"])
 def export_circuit():
     data = request.json
-    option = data["option"]
+    language = data["option"]
     circuit = data["circuit"]
     try:
         wrapper = CircuitWrapper(qiskit_instructions=circuit)
-        option_lower = option.lower()
-        if option_lower == "quil":
-            output = wrapper.export_quil()
-        elif option_lower == "pyquil":
-            output = wrapper.export_pyquil()
-        elif option_lower == "openqasm":
-            output = wrapper.export_qasm()
-        elif option_lower == "qiskit":
-            output = wrapper.export_qiskit_commands()
-        elif option_lower == "cirq":
-            output = wrapper.export_cirq_json()
-        elif option_lower == "braket":
-            output = wrapper.export_braket_ir()
-        elif option_lower == "qsharp":
-            output = wrapper.export_qsharp()
-        elif option_lower == "quirk":
-            output = wrapper.export_quirk()
-        else:
-            return "Bad Request!", 400
+        output = wrapper.export_language(language)
+    except ValueError:
+        return "Bad Request!", 400
     except DeviceError as de:
-        gate = re.findall(r'(?:Gate )(.*?)(?: not)', str(de))[0]
-        return f"Exporting {option} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:Gate )(.*?)(?: not)", str(de))[0]
+        return f"Exporting {language} does not support '{gate}' gates", 500
     except QasmException as qe:
         gate = re.findall(r'"(.*?)"', qe.message)[0]
-        return f"Exporting {option} does not support '{gate}' gates", 500
+        return f"Exporting {language} does not support '{gate}' gates", 500
     except NotImplementedError as nie:
-        gate = re.findall(r'(?:convert )(.*?)(?: |\()', str(nie))[0]
-        return f"Exporting {option} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:convert )(.*?)(?: |\()", str(nie))[0]
+        return f"Exporting {language} does not support '{gate}' gates", 500
     except SyntaxError as se:
-        line = re.findall(r'(line \d+)', str(se))[0]
+        line = re.findall(r"(line \d+)", str(se))[0]
         return f"Invalid syntax of input in line {line}"
     except Exception as e:
         traceback.print_exc()
         print(str(e))
-        return f"General error while exporting {option}: {str(e)}", 500
+        return f"General error while exporting {language}: {str(e)}", 500
     return output
 
 
-@app.route('/convert', methods=['Post'])
+@app.route("/convert", methods=["Post"])
 def convert():
     data = request.json
-    option = data["option"]
-    option_output = data["optionOutput"]
+    language = data["option"]
+    language_output = data["optionOutput"]
     circuit = data["circuit"]
     try:
         wrapper = CircuitWrapper()
-        option_lower = option.lower()
-        if option_lower == "quil":
-            wrapper.import_quil(circuit)
-        elif option_lower == "pyquil":
-            wrapper.import_pyquil(circuit)
-        elif option_lower == "openqasm":
-            wrapper.import_qasm(circuit)
-        elif option_lower == "qiskit":
-            wrapper.import_qiskit(circuit)
-        elif option_lower == "cirq":
-            wrapper.import_cirq_json(circuit)
-        elif option_lower == "cirqsdk":
-            wrapper.import_cirq(circuit)
-        elif option_lower == "braket":
-            wrapper.import_braket_ir(circuit)
-        elif option_lower == "braketsdk":
-            wrapper.import_braket(circuit)
-        elif option_lower == "qsharp":
-            wrapper.import_qsharp(circuit)
-        elif option_lower == "quirk":
-            wrapper.import_quirk(circuit)
-        else:
-            return "Bad Request!", 400
+        wrapper.import_language(circuit, language)
+    except ValueError:
+        return "Bad Request!", 400
     except DeviceError as de:
-        gate = re.findall(r'(?:Gate )(.*?)(?: not)', str(de))[0]
-        return f"Converting from {option} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:Gate )(.*?)(?: not)", str(de))[0]
+        return f"Converting from {language} does not support '{gate}' gates", 500
     except QasmException as qe:
         gate = re.findall(r'"(.*?)"', qe.message)[0]
-        return f"Converting from {option} does not support '{gate}' gates", 500
+        return f"Converting from {language} does not support '{gate}' gates", 500
     except NotImplementedError as nie:
-        gate = re.findall(r'(?:convert )(.*?)(?: |\()', str(nie))[0]
-        return f"Converting from {option} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:convert )(.*?)(?: |\()", str(nie))[0]
+        return f"Converting from {language} does not support '{gate}' gates", 500
     except SyntaxError as se:
-        line = re.findall(r'(line \d+)', str(se))[0]
+        line = re.findall(r"(line \d+)", str(se))[0]
         return f"Invalid syntax of input in line {line}"
     except Exception as e:
         traceback.print_exc()
         print(str(e))
-        return f"General error while converting from {option}: {str(e)}", 500
+        return f"General error while converting from {language}: {str(e)}", 500
 
     try:
-        option_output_lower = option_output.lower()
-        if option_output_lower == "quil":
-            output = wrapper.export_quil()
-        elif option_output_lower == "pyquil":
-            output = wrapper.export_pyquil_commands()
-        elif option_output_lower == "openqasm":
-            output = wrapper.export_qasm()
-        elif option_output_lower == "qiskit":
-            output = wrapper.export_qiskit_commands(include_imports=True)
-        elif option_output_lower == "cirq" or option_output_lower == "cirq-json":
-            output = wrapper.export_cirq_json()
-        elif option_output_lower == "braket":
-            output = wrapper.export_braket_ir()
-        elif option_output_lower == "qsharp":
-            output = wrapper.export_qsharp()
-        elif option_output_lower == "quirk":
-            output = wrapper.export_quirk()
-        else:
-            return "Bad Request!", 400
+        output = wrapper.export_language(language_output)
+    except ValueError:
+        return "Bad Request!", 400
     except DeviceError as de:
-        gate = re.findall(r'(?:Gate )(.*?)(?: not)', str(de))[0]
-        return f"Converting to {option_output} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:Gate )(.*?)(?: not)", str(de))[0]
+        return f"Converting to {language_output} does not support '{gate}' gates", 500
     except QasmException as qe:
         gate = re.findall(r'"(.*?)"', qe.message)[0]
-        return f"Converting to {option_output} does not support '{gate}' gates", 500
+        return f"Converting to {language_output} does not support '{gate}' gates", 500
     except NotImplementedError as nie:
-        gate = re.findall(r'(?:convert )(.*?)(?: |\()', str(nie))[0]
-        return f"Converting to {option_output} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:convert )(.*?)(?: |\()", str(nie))[0]
+        return f"Converting to {language_output} does not support '{gate}' gates", 500
     except Exception as e:
         traceback.print_exc()
         print(str(e))
-        return f"General error while converting to {option_output}: {str(e)}", 500
-
+        return f"General error while converting to {language_output}: {str(e)}", 500
     return output
 
 
-@app.route('/unroll', methods=['Post'])
+@app.route("/unroll", methods=["Post"])
 def unroll():
     data = request.json
-    option = data["option"]
+    architecture = data["option"]
     circuit = data["circuit"]
-    isExpert = data["isExpert"]
-    format = data["format"]
+    is_custom_export = data["isExpert"]
+    language = data["format"]
 
     try:
         wrapper = CircuitWrapper(qiskit_instructions=circuit)
-        if option == "Rigetti":
+        if architecture == "Rigetti":
             wrapper.unroll_rigetti()
-        elif option == "IBMQ":
+        elif architecture == "IBMQ":
             wrapper.unroll_ibm()
-        elif option == "Sycamore":
+        elif architecture == "Sycamore":
             wrapper.unroll_sycamore()
         else:
             return "Bad Request!", 400
 
-        if isExpert:
-            format_lower = format.lower()
-            if format_lower == "quil":
-                output = wrapper.export_quil()
-            elif format_lower == "pyquil":
-                output = wrapper.export_pyquil()
-            elif format_lower == "openqasm":
-                output = wrapper.export_qasm()
-            elif format_lower == "qiskit":
-                output = wrapper.export_qiskit_commands()
-            elif format_lower == "cirq":
-                output = wrapper.export_cirq_json()
-            elif format_lower == "braket":
-                output = wrapper.export_braket_ir()
-            elif format_lower == "qsharp":
-                output = wrapper.export_qsharp()
-            elif format_lower == "quirk":
-                output = wrapper.export_quirk()
-            else:
+        if is_custom_export:
+            try:
+                wrapper.export_language(language)
+            except ValueError:
                 return "Bad Request!", 400
+
         else:
-            if option == "Rigetti":
+            if architecture == "Rigetti":
                 output = wrapper.export_quil()
-            elif option == "IBMQ":
+            elif architecture == "IBMQ":
                 output = wrapper.export_qasm()
-            elif option == "Syncamore":
+            elif architecture == "Syncamore":
                 output = wrapper.export_cirq_json()
             else:
                 return "Bad Request!", 400
 
     except DeviceError as de:
-        gate = re.findall(r'(?:Gate )(.*?)(?: not)', str(de))[0]
-        return f"Exporting {option} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:Gate )(.*?)(?: not)", str(de))[0]
+        return f"Exporting {architecture} does not support '{gate}' gates", 500
     except QasmException as qe:
         gate = re.findall(r'"(.*?)"', qe.message)[0]
-        return f"Exporting {option} does not support '{gate}' gates", 500
+        return f"Exporting {architecture} does not support '{gate}' gates", 500
     except NotImplementedError as nie:
-        gate = re.findall(r'(?:convert )(.*?)(?: |\()', str(nie))[0]
-        return f"Exporting {option} does not support '{gate}' gates", 500
+        gate = re.findall(r"(?:convert )(.*?)(?: |\()", str(nie))[0]
+        return f"Exporting {architecture} does not support '{gate}' gates", 500
     except SyntaxError as se:
-        line = re.findall(r'(line \d+)', str(se))[0]
+        line = re.findall(r"(line \d+)", str(se))[0]
         return f"Invalid syntax of input in line {line}"
     except Exception as e:
         traceback.print_exc()
         print(str(e))
-        return f"General error while exporting {option}: {str(e)}", 500
+        return f"General error while exporting {architecture}: {str(e)}", 500
     return output
 
 
-@app.route('/simulate', methods=['Post'])
+@app.route("/simulate", methods=["Post"])
 def simulate():
     data = request.json
     circuit = data["circuit"]
@@ -273,12 +186,13 @@ def simulate():
         return str(e), 500
     return output
 
-@app.route('/depth', methods=['Post'])
+
+@app.route("/depth", methods=["Post"])
 def depth():
     data = request.json
     circuit = data["circuit"]
     depth = {}
-    try:        
+    try:
         wrapper = CircuitWrapper(qiskit_instructions=circuit)
         wrapper.unroll_ibm()
         depth["q_depth"] = wrapper.depth()
@@ -314,17 +228,17 @@ def depth():
     return output
 
 
-@app.route('/depth_comparison_qpu', methods=['Post'])
+@app.route("/depth_comparison_qpu", methods=["Post"])
 def depth_comparison_qpu():
     data = request.json
     circuit = data["circuit"]
     depth = {}
-    try: 
+    try:
         wrapper = CircuitWrapper(qiskit_instructions=circuit)
         circuit = wrapper.circuit
         qpu_map = qpus()
-        for qpu_name in qpu_map:      
-            wrapper = CircuitWrapper(qiskit_circuit=circuit)      
+        for qpu_name in qpu_map:
+            wrapper = CircuitWrapper(qiskit_circuit=circuit)
             is_ibm = False
             qpu = qpu_map[qpu_name]
             if qpu[1] == "q":
@@ -351,5 +265,5 @@ def depth_comparison_qpu():
     return output
 
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=5012, debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5012, debug=False)
